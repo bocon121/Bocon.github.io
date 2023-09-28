1897b5aaf08209ff1ce72d9276b0f684b0b727ca
bocon121 <13277204+bocon121@users.noreply.github.com>
2023-09-28 23:08:53 +1000

    Push

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ document.addEventListener("DOMContentLoaded", function () {
         sectionObserver.observe(section);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Show/hide the "Back to Top" button
+    const backToTopButton = document.getElementById("back-to-top");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    // Scroll to top when the button is clicked
+    backToTopButton.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
