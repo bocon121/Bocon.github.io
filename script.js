96883290549a6e6a29682c777a86e2002a01fd26
bocon121 <13277204+bocon121@users.noreply.github.com>
2023-09-28 22:48:31 +1000

    Javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hide the welcome screen when the content is fully loaded
+    setTimeout(function () {
+        document.querySelector(".welcome-screen").style.display = "none";
+    }, 2000); // Adjust the delay (in milliseconds) as needed
+
+    // Scroll Animation
+    const sections = document.querySelectorAll("section");
+    const options = {
+        threshold: 0.7,
+    };
+
+    const sectionObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add("scroll-animation");
+            observer.unobserve(entry.target);
+        });
+    }, options);
+
+    sections.forEach((section) => {
+        sectionObserver.observe(section);
+    });
+});
